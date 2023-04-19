160f9d4ebbae3586a3092a577e9e188d735c10b0
stasc <schalyi1@stu.vistula.edu.pl>
2023-04-19 18:45:24 +0200

    Models

--- src/main/java/com/quiz/web/models/Quiz.java
+++ src/main/java/com/quiz/web/models/Quiz.java
@@ -1,15 +1,13 @@
 package com.quiz.web.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -17,14 +15,27 @@ import java.time.LocalDateTime;
 @Builder
 @Entity
 @Table(name="quiz")
+//the quiz class
 public class Quiz {
     @Id
     private long id;
+    @NonNull
     private String title;
     private String photoUrl;
+    @Nullable
     private String content;
     @CreationTimestamp
     private LocalDateTime createOn;
     @CreationTimestamp
     private  LocalDateTime updateOn;
+
+    @ManyToMany
+    @JoinTable(
+            name = "quiz_category",
+            joinColumns = @JoinColumn(name = "quiz_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private List<Category> categories = new ArrayList<Category>();
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Question> questions = new ArrayList<>();
+
 }
